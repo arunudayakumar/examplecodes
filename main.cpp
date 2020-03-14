33f85342d98bbce2384adc7229195c7f578e6c7e
arun <arunu2002@gmail.com>
2020-03-14 19:13:40 +0530

    String

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  char s[]="Null Terminated String";
+
+  int i=0;
+
+  while(s[i])
+  {
+    cout<<s[i];
+    i++;
+  }
+
+
+    return 0;
+}
